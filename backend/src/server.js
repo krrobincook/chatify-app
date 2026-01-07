12b80c8522603fef0fccc9ae66546aaa2d150441
krrobincook <kumarrobincook@gmail.com>
2026-01-07 21:39:15 +0530

    Update auth routes and server logic

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,9 +2,9 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import path from'path'
-
 import authRoutes from "./routes/auth.route.js"
 import messageRoutes from "./routes/message.route.js"
+import { connectDB } from '../lib/db.js'
 
 dotenv.config();
 
@@ -12,6 +12,8 @@ const app = express()
 const __dirname = path.resolve();
 
 const PORT = process.env.PORT || 3000
+
+app.use(express.json());
 
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
@@ -27,4 +29,7 @@ if(process.env.NODE_ENV === 'production'){
     })
 }
 
-app.listen(PORT, () => console.log("Server running at port: " + PORT))
+app.listen(PORT, () => {
+    console.log("Server running at port: " + PORT)
+    connectDB();
+})
