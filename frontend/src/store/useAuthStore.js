d50df4db750e4af0397b07e65e023b92dd1cd713
krrobincook <kumarrobincook@gmail.com>
2026-01-12 22:34:53 +0530

    add loading state

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,9 +3,9 @@ import { create } from "zustand";
 export const useAuthStore = create((set) => ({
     authUser: { name: "Robin", _id: 123, age: 20 },
     isLoggedIn: false,
-
+    isLoading: false,
     login: () => {
         console.log("We just logged in");
-        set({ isLoggedIn: true });
+        set({ isLoggedIn: true, isLoading: true });
     }
 }));
