cac6383d81a2d40c4ebdcddc9f5a752da09b2219
krrobincook <kumarrobincook@gmail.com>
2026-01-13 21:20:20 +0530

    fix the error

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,14 +6,15 @@ import messageRoutes from "./routes/message.route.js"
 import { connectDB } from './lib/db.js'
 import {ENV} from "./lib/env.js"
 import cookieParser from "cookie-parser"
+import cors from "cors";
 const app = express()
 const __dirname = path.resolve();
 
 const PORT = ENV.PORT || 3001
 
 app.use(express.json());
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }))
 app.use(cookieParser());
-
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
 
