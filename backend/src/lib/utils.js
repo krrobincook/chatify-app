3725f3bbd3fc745dfc7f279530d0902f089a584b
krrobincook <kumarrobincook@gmail.com>
2026-01-20 23:53:05 +0530

    fix internal server error

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -7,8 +7,8 @@ export const generateToken = (userId, res) => {
 
   res.cookie("jwt", token, {
     httpOnly: true,
-    sameSite: "lax",   // REQUIRED for socket + axios
-    secure: false,    // true ONLY in HTTPS production
+    sameSite: ENV.NODE_ENV === "development" ? "lax" : "none",
+    secure: ENV.NODE_ENV !== "development",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 
--- backend/src/middleware/arcjet.middleware.js
+++ backend/src/middleware/arcjet.middleware.js
@@ -3,27 +3,27 @@ import aj from "../lib/arcjet.js";
 import { isSpoofedBot } from "@arcjet/inspect";
 
 export const arcjetProtection = async (req, res, next) => {
-    try{
+    try {
         const decision = await aj.protect(req);
-        if(decision.isDenied()) {
-            if(decision.reason.isRateLimit()) {
-                return res.status(429).json({message: "Rate limit exceeded. Please try again later."} );
-            } else if(decision.reason.isBot()) {
-                return res.status(403).json({messaage: "Bot access denied."});
+        if (decision.isDenied()) {
+            if (decision.reason.isRateLimit()) {
+                return res.status(429).json({ message: "Rate limit exceeded. Please try again later." });
+            } else if (decision.reason.isBot()) {
+                return res.status(403).json({ messaage: "Bot access denied." });
             } else {
-                return response.status(403).json({messaage: "Access denied by security policy"});
+                return res.status(403).json({ messaage: "Access denied by security policy" });
             }
         }
 
-        if(decision.results.some(isSpoofedBot)){
+        if (decision.results.some(isSpoofedBot)) {
             return res.status(403).json({
                 error: "Spoofed bot detected",
                 messaage: "Malicious bot activity detected.",
             })
         }
         next()
-    } catch(error) {
-      console.log("Arcjet Protection Error:", error);
-      next();
+    } catch (error) {
+        console.log("Arcjet Protection Error:", error);
+        next();
     }
 }
